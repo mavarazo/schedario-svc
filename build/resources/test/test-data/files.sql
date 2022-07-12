0bc9f20a263f4a6b692449063d7dc1f799010e8c
Mav <marco.niederberger@gmail.com>
2022-07-12 20:52:25 +0200

    !!! cleanup

--- build/resources/test/test-data/files.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO files (id, created_date, modified_date, title, notes, checksum, path, size, created) VALUES (1, current_timestamp, current_timestamp, '2020-Scrum-Guide-US', null, 1, '/2020-Scrum-Guide-US.pdf', 1,        current_timestamp);
